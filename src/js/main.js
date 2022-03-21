5e886f27fe68d7c38f655f11de02e4a84ddb524b
fauverick <hungnc888@gmail.com>
2022-03-21 14:16:31 +0700

    sua nav bar tren phone

--- src/js/main.js
+++ src/js/main.js
@@ -33,6 +33,9 @@ let navbar = document.getElementById("nav-bar");
 let closebar = document.getElementById("close-bar");
 let body = document.querySelector("body");
 let linklist = document.getElementById("link-list");
+let nav_item = document.querySelectorAll(".nav_category");
+
+
 
 let flag = false;
 
@@ -51,6 +54,19 @@ closebar.onclick = function () {
     bar.style.visibility = "inherit";
   }, 200);
 };
+
+
+
+nav_item.forEach(function(btn){
+  btn.addEventListener('click', function() {
+    navbar.style.width = "0px";
+    body.style.overflowY = "inherit";
+    linklist.style.display = "none";
+    setTimeout(function () {
+      bar.style.visibility = "inherit";
+    }, 200);
+  })
+})
 
 
 
